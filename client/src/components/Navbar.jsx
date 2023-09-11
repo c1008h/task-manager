a7a1a48bbc048ad8c4374b16cb1c0ab3bcfc6eb0
Chris Hong <hongchris97@gmail.com>
2023-09-10 21:57:05 -0700

    nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+        <ul>
+            <li>Homepage</li>
+        </ul>
+    </nav>
   )
 }
